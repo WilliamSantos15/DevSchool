8e9b05de23e267c2a68359648211b78fb2db4041
William <williamsantos041015@gmail.com>
2021-09-17 21:02:38 -0300

    Ufa

---
+++ api/src/index.js
@@ -0,0 +1,71 @@
+import db from './db.js';
+import express from 'express'
+import cors from 'cors'
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/matricula', async (req, resp) => {
+    try{
+        let r = await db.tb_matricula.findAll({ order: [['id_matricula', 'desc']]});
+        resp.send(r)
+    } catch (e) {
+        resp.send(e.toString)
+    }
+})
+
+app.post('/matricula', async (req, resp) => {
+    let { nome, chamada, curso, turma } = req.body;
+
+    let alunoOk = await db.tb_matricula.findOne({where: {nm_turma: turma, nr_chamada: chamada}})
+        if  (alunoOk !== null)
+            return resp.send({erro: '❌ Aluno já inserido'})
+
+    try{
+        let r = await db.tb_matricula.create({
+            nm_aluno: nome,
+            nr_chamada: chamada,
+            nm_curso: curso,
+            nm_turma: turma
+        })
+        resp.send(r);
+    } catch (e) {
+        resp.send({ erro: "Deu erro" })
+    }
+})
+
+ app.put('/matricula/:id', async (req, resp) => {
+     try{
+        let {id} = req.params;
+        let { nome } = req.body;
+
+         let r = await db.tb_matricula.update(
+         {
+             nm_aluno: nome
+         },
+         {
+             where: { id_matricula: id }
+         });
+
+         resp.sendStatus(200);
+     } catch (e) {
+         resp.send({ erro: e.toString() })
+     }
+
+})
+
+app.delete('/matricula/:id', async (req, resp) => {
+    try{
+        let {id} = req.params;
+
+        let r = await db.tb_matricula.destroy({ where: { id_matricula: id }  })
+        resp.sendStatus(200);
+    } catch (e) {
+        resp.send({ erro: e.toString() })
+    }
+})
+
+
+app.listen(process.env.PORT,
+
+x => console.log(`Server up at port ${process.env.PORT}`))
---
+++ react/site/src/pages/alunos/index.js
@@ -0,0 +1,202 @@
+
+import Cabecalho from '../../components/cabecalho'
+import Menu from '../../components/menu'
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { confirmAlert } from 'react-confirm-alert';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+
+import LoadingBar from 'react-top-loading-bar';
+
+import { Container, Conteudo } from './styled'
+
+import { useState, useEffect, React, useRef } from 'react';
+
+import Api from '../../service/api';
+const api = new Api();
+
+
+export default function Index() {
+
+    const [alunos, setAlunos] = useState([]);
+    const [nome, setNome] = useState('');
+    const [chamada, setChamada] = useState('');
+    const [turma, setTurma] = useState('');
+    const [curso, setCurso] = useState('');
+    const [idAlterando, setIdAlterando] = useState(0);
+    const loading = useRef();
+
+
+    async function listar() {
+        loading.current.continuousStart();
+        let r = await api.listar();
+        setAlunos(r);
+        loading.current.complete();
+    }
+
+    useEffect(() => {
+        listar();
+    },
+        [])
+
+    async function inserir() {
+        loading.current.continuousStart();
+        if (nome !== ('') && chamada > 0 && turma !== ('') && turma === (NaN) && curso !== ("") && curso === (NaN)){
+
+            if (idAlterando == 0 ) {
+                let r = await api.inserir(nome, chamada, curso, turma);
+                
+                if (r.erro)
+                    toast.dark(r.erro);
+                else
+                    toast.dark('✅ Aluno inserido!');
+            } else {
+                let r = await api.alterar(idAlterando, nome, chamada, curso, turma);
+
+                if (r.erro)
+                    toast.dark(r.erro);
+                else
+                    toast.dark('✅ Aluno alterado!');
+            }
+
+        } else (
+            toast.dark('❌ Campos inválidos')
+        )
+        limparCampos()
+        listar()
+        loading.current.complete();
+    }
+
+    function limparCampos() {
+        setNome('');
+        setChamada('');
+        setCurso('');
+        setTurma('');
+        setIdAlterando(0);
+
+    }
+
+    async function remover(id) {
+            loading.current.continuousStart();
+            confirmAlert({
+                title: 'Remover Aluno',
+                message: `Tem certeza que deseja remover o Aluno ${id}?`,
+                buttons: [
+                    {
+                        label: 'Sim, Remover',
+                        onClick: async() => {
+                            let r = await api.remover(id)
+                            if(r.erro) {
+                                toast.dark(r.erro)
+                            } else {
+                                toast.dark('✅ Aluno Removido')
+                            }
+                            listar();
+                        }
+                    },
+                    {
+                        label: 'Não, Cancelar'
+                    }
+                ]
+    
+            })
+            listar()
+    
+            loading.current.complete()
+        }
+
+    async function editar(item) {
+        setNome(item.nm_aluno);
+        setChamada(item.nr_chamada);
+        setCurso(item.nm_curso);
+        setTurma(item.nm_turma);
+        setIdAlterando(item.id_matricula);
+    }
+
+    return (
+        <Container>
+        <ToastContainer/>
+        <LoadingBar color="#EA10C7" ref={loading} />
+            <Menu />
+            <Conteudo>
+                <Cabecalho />
+                <div class="body-right-box">
+                    <div class="new-student-box">
+                        
+                        <div class="text-new-student">
+                            <div class="bar-new-student"></div>
+                            <div class="text-new-student"> {idAlterando == 0 ? "Novo Aluno" : "Alterando Aluno " + idAlterando}</div>
+                        </div>
+
+                        <div class="input-new-student"> 
+                            <div class="input-left">
+                                <div class="agp-input"> 
+                                    <div class="name-student"> Nome: </div>  
+                                    <div class="input"> <input type="text" value={nome} onChange={e => setNome(e.target.value)} /> </div>  
+                                </div> 
+                                <div class="agp-input">
+                                    <div class="number-student"> Chamada: </div>  
+                                    <div class="input"> <input type="text" value={chamada} onChange={e => setChamada(e.target.value)} /> </div> 
+                                </div>
+                            </div>
+
+                            <div class="input-right">
+                                <div class="agp-input">
+                                    <div class="corse-student"> Curso: </div>  
+                                    <div class="input"> <input type="text" value={curso} onChange={e => setCurso(e.target.value)} /> </div>  
+                                </div>
+                                <div class="agp-input">
+                                    <div class="class-student"> Turma: </div>  
+                                    <div class="input"> <input type="text" value={turma} onChange={e => setTurma(e.target.value)} /> </div> 
+                                </div>
+                            </div>
+                            <div class="button-create"> <button onClick={inserir}> {idAlterando == 0 ? "Cadastrar" : "Alterar"} </button> </div>
+                        </div>
+                    </div>
+
+                    <div class="student-registered-box">
+                        <div class="row-bar"> 
+                            <div class="bar-new-student"> </div>
+                            <div class="text-registered-student"> Alunos Matriculados </div>
+                        </div>
+                    
+                        <table class ="table-user">
+                            <thead>
+                                <tr>
+                                    <th> ID </th>
+                                    <th> Nome </th>
+                                    <th> Chamada </th>
+                                    <th> Turma </th>
+                                    <th> Curso </th>
+                                    <th class="coluna-acao"> </th>
+                                    <th class="coluna-acao"> </th>
+                                </tr>
+                            </thead>
+                    
+                            <tbody>
+
+                            {alunos.map((item, i) =>
+
+                                <tr className={i % 2 == 0 ? "linha-alternada" : ""}>
+                                    <td> {item.id_matricula} </td>
+                                    <td title={item.nm_aluno}> {item.nm_aluno != null && item.nm_aluno.length >= 25 
+                                            ? item.nm_aluno.substr(0, 25) + '...' 
+                                            : item.nm_aluno} </td>
+                                    <td> {item.nr_chamada} </td>
+                                    <td> {item.nm_turma} </td>
+                                    <td> {item.nm_curso} </td>
+                                    <td className="coluna-acao"> <button onClick={() => editar(item)}> <img src="/assets/images/edit.svg" alt="" /> </button> </td>
+                                    <td className="coluna-acao"> <button onClick={() => remover(item.id_matricula)}> <img src="/assets/images/trash.svg" alt="" /> </button> </td>
+                                </tr>
+                            )}
+                                
+                            </tbody> 
+                        </table>
+                    </div>
+                </div>
+            </Conteudo>
+        </Container>
+    )
+}
